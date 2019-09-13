6fa2fe1701f22d1822bb40e1f7e8f3c91e9d88b8
rodrige <peniouatt@hotmail.fr>
2019-09-13 17:38:44 +0200

    first commit

---
+++ pokemon-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PokeApi from './components/pokeApi';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PokemeonDate from './components/pokemonsData';
+import pokecss from './css';
+
+ReactDOM.render(<PokemeonDate/>,document.getElementById('root'))
+
+
